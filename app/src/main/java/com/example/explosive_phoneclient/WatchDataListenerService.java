8036173e299a8e3f92e2ddbb7af5dd5dd4d1a71f
akshansh <akshsagar1234@gmail.com>
2026-05-10 16:23:01 +0530

    2nd commit - Added code to receive data from watch

---
+++ app/src/main/java/com/example/explosive_phoneclient/WatchDataListenerService.java
@@ -0,0 +1,36 @@
+package com.example.explosive_phoneclient;
+
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WatchDataListenerService
+        extends WearableListenerService {
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+        super.onMessageReceived(messageEvent);
+
+        if (messageEvent.getPath().equals("/punch_data")) {
+
+            String data =
+                    new String(messageEvent.getData());
+
+            Log.d("PHONE_RECEIVED", data);
+
+            Intent intent =
+                    new Intent("PUNCH_DATA");
+
+            intent.putExtra("data", data);
+
+            LocalBroadcastManager
+                    .getInstance(this)
+                    .sendBroadcast(intent);
+        }
+    }
+}
